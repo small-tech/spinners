0a71b8280404b2ea5843e7c040396747a811df2a
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-07-04 15:45:16 +0100

    Make default color currentColor

--- src/Dots.svelte
+++ src/Dots.svelte
@@ -2,7 +2,7 @@
   // Spinner SVG is from the Ionic Framework
   // (https://ionicframework.com/docs/api/spinner via https://codepen.io/ionic/pen/GgwVON)
   export let size = 32
-  export let colour = 'black'
+  export let colour = 'currentColor'
   export let show = true
 </script>
 
