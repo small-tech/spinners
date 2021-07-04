f3da07f4819cef56b3fb737615d3dcef45dee423
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-07-04 15:26:39 +0100

    Generate lines animation SVG with code

--- src/Lines.svelte
+++ src/Lines.svelte
@@ -1,11 +1,36 @@
 <script>
-  // Spinner SVG is from the Ionic Framework
+  // Spinner is based on the Ionic Framework
   // (https://ionicframework.com/docs/api/spinner via https://codepen.io/ionic/pen/GgwVON)
   export let size = 32
   export let colour = 'black'
   export let show = true
+
+  // Lines animation
+  const values = {
+    angles: [180, 210, 240, 270, 300, 330, 0, 30, 60, 90, 120, 150],
+    opacityKeyframes: [1, .85, .7, .65, .55, .45, .35, .25, .15, .1, 0]
+  }
+  const lines = []
+  values.angles.forEach((angle, index) => {
+    let opacityKeyframes = [...values.opacityKeyframes]
+    opacityKeyframes = opacityKeyframes.splice(opacityKeyframes.length-index).concat(opacityKeyframes) // Rotate the opacity keyframe values, once per line.
+    opacityKeyframes.push(opacityKeyframes[0])                                           // Repeat the initial opacity.
+    opacityKeyframes = opacityKeyframes.join(';')
+    lines.push({
+      angle,
+      opacityKeyframes
+    })
+  })
 </script>
 
 {#if show}
-  <svg height={size} stroke={colour} viewBox="0 0 64 64"><g stroke-width="4" stroke-linecap="round"><line y1="12" y2="20" transform="translate(32,32) rotate(180)"><animate attributeName="stroke-opacity" dur="750ms" values="1;.85;.7;.65;.55;.45;.35;.25;.15;.1;0;1" repeatCount="indefinite"></animate></line><line y1="12" y2="20" transform="translate(32,32) rotate(210)"><animate attributeName="stroke-opacity" dur="750ms" values="0;1;.85;.7;.65;.55;.45;.35;.25;.15;.1;0" repeatCount="indefinite"></animate></line><line y1="12" y2="20" transform="translate(32,32) rotate(240)"><animate attributeName="stroke-opacity" dur="750ms" values=".1;0;1;.85;.7;.65;.55;.45;.35;.25;.15;.1" repeatCount="indefinite"></animate></line><line y1="12" y2="20" transform="translate(32,32) rotate(270)"><animate attributeName="stroke-opacity" dur="750ms" values=".15;.1;0;1;.85;.7;.65;.55;.45;.35;.25;.15" repeatCount="indefinite"></animate></line><line y1="12" y2="20" transform="translate(32,32) rotate(300)"><animate attributeName="stroke-opacity" dur="750ms" values=".25;.15;.1;0;1;.85;.7;.65;.55;.45;.35;.25" repeatCount="indefinite"></animate></line><line y1="12" y2="20" transform="translate(32,32) rotate(330)"><animate attributeName="stroke-opacity" dur="750ms" values=".35;.25;.15;.1;0;1;.85;.7;.65;.55;.45;.35" repeatCount="indefinite"></animate></line><line y1="12" y2="20" transform="translate(32,32) rotate(0)"><animate attributeName="stroke-opacity" dur="750ms" values=".45;.35;.25;.15;.1;0;1;.85;.7;.65;.55;.45" repeatCount="indefinite"></animate></line><line y1="12" y2="20" transform="translate(32,32) rotate(30)"><animate attributeName="stroke-opacity" dur="750ms" values=".55;.45;.35;.25;.15;.1;0;1;.85;.7;.65;.55" repeatCount="indefinite"></animate></line><line y1="12" y2="20" transform="translate(32,32) rotate(60)"><animate attributeName="stroke-opacity" dur="750ms" values=".65;.55;.45;.35;.25;.15;.1;0;1;.85;.7;.65" repeatCount="indefinite"></animate></line><line y1="12" y2="20" transform="translate(32,32) rotate(90)"><animate attributeName="stroke-opacity" dur="750ms" values=".7;.65;.55;.45;.35;.25;.15;.1;0;1;.85;.7" repeatCount="indefinite"></animate></line><line y1="12" y2="20" transform="translate(32,32) rotate(120)"><animate attributeName="stroke-opacity" dur="750ms" values=".85;.7;.65;.55;.45;.35;.25;.15;.1;0;1;.85" repeatCount="indefinite"></animate></line><line y1="12" y2="20" transform="translate(32,32) rotate(150)"><animate attributeName="stroke-opacity" dur="750ms" values="1;.85;.7;.65;.55;.45;.35;.25;.15;.1;0;1" repeatCount="indefinite"></animate></line></g></svg>
+  <svg height={size} stroke={colour} viewBox="0 0 64 64">
+    <g stroke-width="4" stroke-linecap="round">
+      {#each lines as line}
+        <line y1="12" y2="20" transform="translate(32,32) rotate({line.angle})">
+          <animate attributeName="stroke-opacity" dur="750ms" values={line.opacityKeyframes} repeatCount="indefinite"></animate>
+        </line>
+      {/each}
+    </g>
+  </svg>
 {/if}
